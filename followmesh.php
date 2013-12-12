74134fa1f93fac547d456e19c7782d3b7dcabe35
Dmytri Kleiner <dk@trick.ca>
2013-12-12 11:48:21 +0100

    whitespace

--- followmesh.php
+++ followmesh.php
@@ -4,23 +4,23 @@
  *
  * Distributed Social Media, Powered by WordPress
  *
- * @package		FollowMesh
- * @author		Dmytri Kleiner <dk@trick.ca>
- * @license		GPL-2.0+
- * @link			https://github.com/tricknik/followmesh
+ * @package FollowMesh
+ * @author Dmytri Kleiner <dk@trick.ca>
+ * @license GPL-2.0+
+ * @link https://github.com/tricknik/followmesh
  * @copyright 2013 Automattic
  *
  * @wordpress-plugin
- * Plugin Name:				FollowMesh
- * Plugin URI:				https://github.com/tricknik/followmesh
- * Description:				Distributed Social Media, Powered by WordPress
- * Version:						0.1.0
- * Author:						Dmytri Kleiner
- * Author URI:				http://dmytri.info
- * Text Domain:				followmesh
- * License:						GPL-2.0+
- * License URI:				http://www.gnu.org/licenses/gpl-2.0.txt
- * Domain Path:				/languages
+ * Plugin Name: FollowMesh
+ * Plugin URI: https://github.com/tricknik/followmesh
+ * Description: Distributed Social Media, Powered by WordPress
+ * Version: 0.1.0
+ * Author: Dmytri Kleiner
+ * Author URI: http://dmytri.info
+ * Text Domain: followmesh
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path: /languages
  * GitHub Plugin URI: https://github.com/tricknik/followmesh
  */
 
